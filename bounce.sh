076587c9d0d52ba477e5c5ec2f22315c4b5f23d0
David Leander Boelke <david.boelke@rwth-aachen.de>
2021-08-06 14:30:59 +0200

    added comments and changed starting direction

--- bounce.sh
+++ bounce.sh
@@ -1,17 +1,39 @@
+## get window id
+
+## OPTIONAL LINE
+# the following can be used, so the id of the currently active (focused) window is taken
 #WINDOW=$(printf 0x%x $(xdotool getactivewindow) )
+
+# this command however launches xwininfo and the user can select the window by clicking on it
 WINDOW=$(xwininfo |grep "Window id"|awk '{print $4}')
 
+
+## get general starting information
+# where are the borders of the screen?
 BORDER_H=$(xdpyinfo|grep dimension|awk '{print $2}'|cut -d "x" -f 1)
 BORDER_V=$(xdpyinfo|grep dimension|awk '{print $2}'|cut -d "x" -f 2)
 
+# how large is the window?
 WIDTH=$(xwininfo -id $WINDOW|grep Width|awk '{print $2}')
 HEIGHT=$(xwininfo -id $WINDOW|grep Height|awk '{print $2}')
+# and where is the window?
 X_PLACE=$(xwininfo -id $WINDOW|grep "Absolute upper-left X"|awk '{print $4}')
 Y_PLACE=$(xwininfo -id $WINDOW|grep "Absolute upper-left Y"|awk '{print $4}')
 
+
+## Parameters
+# how long to wait between each step of moving the window?
 WAITTIME=0.001
+# how long are the stepe (in pixels)?
 STEP=2
 
+
+## FUNCTION move
+# parses the input of the function by searching for characters, that describe the movement direction
+# u: up
+# d: down
+# l: left
+# r: right
 move(){
 	if [[ $1 == *u* ]]; then
 		Y_PLACE=`expr $Y_PLACE - $STEP`
@@ -26,9 +48,25 @@ move(){
 	xdotool windowmove $WINDOW $X_PLACE $Y_PLACE
 	sleep $WAITTIME
 }
+
+
+## FUNCTION update direction
+# checks for collisions and updates the movement direction accordingly
+# MOVE==0 up   left
+# MOVE==1 down left
+# MOVE==2 down right
+# MOVE==3 up   right
 updatedirection(){
+## use the follwong lines if you constatntly want to update what the size of the window is
+# if you resize the window as it is moving these lines will allow for collisions with the new window size
 #WIDTH=$(xwininfo -id $WINDOW|grep Width|awk '{print $2}')
 #HEIGHT=$(xwininfo -id $WINDOW|grep Height|awk '{print $2}')
+
+
+## Paddings
+# each movement direction has their pair of two paddings
+# imagine moving the window up left, you can collide with the top of the screen and/or the left of the screen
+# the padding tells you how much space you have left to the top (ONE_PADDING) and the left (TWO_PADDING)
 	if [ $MOVE == 0 ]; then
 		ONE_PADDING=$Y_PLACE
 		TWO_PADDING=$X_PLACE
@@ -43,6 +81,9 @@ updatedirection(){
 		TWO_PADDING=$Y_PLACE
 	fi
 
+
+## Collision
+# in the case that one of the paddings is lower or equal to zero calculate the new direction of movement by adding or substracting from the current movement direction
 	if [ $ONE_PADDING -le 0 ] && [ $TWO_PADDING -le 0 ]; then
 		MOVE=`expr $MOVE + 2`
 	elif [ $ONE_PADDING -le 0 ] && [ $TWO_PADDING -ge 0 ]; then
@@ -51,6 +92,10 @@ updatedirection(){
 		MOVE=`expr $MOVE + 3`
 	fi 
 
+
+## Fix direction
+# updating the direction like in the section about collisions can lead to directions, that are outside the range from 0 to 3
+# we fix the direction by moving it back into the range my means of modulo
 	if [ $MOVE == -1 ]; then
 		MOVE=3
 	elif [ $MOVE == -2 ]; then
@@ -66,7 +111,14 @@ updatedirection(){
 	fi
 }
 
-MOVE=3
+
+## Main
+
+## Starting direction
+MOVE=0
+
+## main loop
+# each loop we update the direction and then perform the direction
 while true
 do
 	updatedirection
